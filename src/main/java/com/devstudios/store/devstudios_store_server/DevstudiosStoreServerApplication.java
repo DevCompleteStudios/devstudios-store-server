fca0a397e53c8801db134a2282e10abb4f1c7423
DevCompleteStudios <jorge.yael333@gmail.com>
2024-11-03 13:58:25 -0600

    First commit

---
+++ src/main/java/com/devstudios/store/devstudios_store_server/DevstudiosStoreServerApplication.java
@@ -0,0 +1,13 @@
+package com.devstudios.store.devstudios_store_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevstudiosStoreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevstudiosStoreServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devstudios/store/devstudios_store_server/DevstudiosStoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstudios.store.devstudios_store_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevstudiosStoreServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
