f4919d29f9560dcfad02dc65dc677ec92d6e3e17
DevCompleteStudios <jorge.yael333@gmail.com>
2024-11-04 09:02:49 -0600

    Agregando auth controller y properties

---
+++ src/main/java/com/devstudios/store/devstudios_store_server/presentation/controllers/AuthController.java
@@ -0,0 +1,14 @@
+package com.devstudios.store.devstudios_store_server.presentation.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+
+
+}
