345bdcf83df34254fb35429c2d90ff9622f00079
DevCompleteStudios <jorge.yael333@gmail.com>
2024-11-04 19:06:29 -0600

    Agregando cascaron de los interceptores que vamos a agregar

---
+++ src/main/java/com/devstudios/store/devstudios_store_server/presentation/interceptors/HandleErrorsInterceptor.java
@@ -0,0 +1,9 @@
+package com.devstudios.store.devstudios_store_server.presentation.interceptors;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+
+
+
+@ControllerAdvice
+public class HandleErrorsInterceptor {}
---
+++ src/main/java/com/devstudios/store/devstudios_store_server/presentation/interceptors/RequestLimitInterceptor.java
@@ -0,0 +1,7 @@
+package com.devstudios.store.devstudios_store_server.presentation.interceptors;
+
+
+
+
+
+public class RequestLimitInterceptor {}
