77c06cf30886151aac2518c8f55b7c99ccccde8a
DevCompleteStudios <jorge.yael333@gmail.com>
2024-11-05 08:36:44 -0600

    Agregando interceptor para manejo de errores personalizados y llaves duplicadas

--- src/main/java/com/devstudios/store/devstudios_store_server/presentation/interceptors/HandleErrorsInterceptor.java
+++ src/main/java/com/devstudios/store/devstudios_store_server/presentation/interceptors/HandleErrorsInterceptor.java
@@ -1,9 +1,49 @@
 package com.devstudios.store.devstudios_store_server.presentation.interceptors;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devstudios.store.devstudios_store_server.infrastructure.CustomExceptions.CustomException;
 
 
 
 
 @ControllerAdvice
-public class HandleErrorsInterceptor {}
+public class HandleErrorsInterceptor {
+
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<?> handleCustomException( CustomException ex ){
+        return getResponse(ex.getError(), ex.getStatus());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> handleDuplicatedKeys( DataIntegrityViolationException ex ){
+        String[] shortMessage = ex.getMessage().split("\\(");
+
+        String field = shortMessage[1].split("\\)")[0];
+        String value = shortMessage[2].split("\\)")[0];
+        String message = field + ": " + value + " already exists";
+
+        return getResponse(message, 400);
+    }
+
+
+    private ResponseEntity<?> getResponse( Object err, int status ){
+        Map<String, Object> res = new HashMap<>();
+
+        res.put("date", LocalDateTime.now());
+        res.put("status", status);
+        res.put("err", err);
+        res.put("message", "error");
+
+        return ResponseEntity.status(status).body(res);
+    }
+
+}
