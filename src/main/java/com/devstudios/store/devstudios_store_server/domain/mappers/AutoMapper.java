140c0684e249285a3bece202fc14402316d3048c
DevCompleteStudios <jorge.yael333@gmail.com>
2024-11-05 20:38:29 -0600

    Auto mapper para los scripts agregado

--- src/main/java/com/devstudios/store/devstudios_store_server/domain/mappers/AutoMapper.java
+++ src/main/java/com/devstudios/store/devstudios_store_server/domain/mappers/AutoMapper.java
@@ -5,14 +5,20 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import com.devstudios.store.devstudios_store_server.application.dtos.key.KeyDto;
+import com.devstudios.store.devstudios_store_server.application.dtos.script.ScriptPreviewDto;
+import com.devstudios.store.devstudios_store_server.application.dtos.script.ScriptPurchaseDto;
 import com.devstudios.store.devstudios_store_server.application.dtos.subscription.SubscriptionPreviewDto;
 import com.devstudios.store.devstudios_store_server.application.dtos.subscription.SubscriptionPurchaseDto;
 import com.devstudios.store.devstudios_store_server.application.dtos.user.UserDto;
 import com.devstudios.store.devstudios_store_server.application.interfaces.projections.IKeyProjection;
+import com.devstudios.store.devstudios_store_server.application.interfaces.projections.IScriptPreviewProjection;
+import com.devstudios.store.devstudios_store_server.application.interfaces.projections.IScriptPurchaseProjection;
 import com.devstudios.store.devstudios_store_server.application.interfaces.projections.ISubscriptionPreviewProjection;
 import com.devstudios.store.devstudios_store_server.application.interfaces.projections.ISubscriptionPurchaseProjection;
 import com.devstudios.store.devstudios_store_server.application.interfaces.projections.IUserProjection;
 import com.devstudios.store.devstudios_store_server.domain.entities.KeyEntity;
+import com.devstudios.store.devstudios_store_server.domain.entities.ScriptEntity;
+import com.devstudios.store.devstudios_store_server.domain.entities.ScriptPurchaseEntity;
 import com.devstudios.store.devstudios_store_server.domain.entities.SubscriptionEntity;
 import com.devstudios.store.devstudios_store_server.domain.entities.SubscriptionPurchaseEntity;
 import com.devstudios.store.devstudios_store_server.domain.entities.UserEntity;
@@ -35,14 +41,45 @@ public class AutoMapper {
 
         List<ISubscriptionPurchaseProjection> subs = userEntity.getSubscriptionPurchases().stream()
             .map( s -> subscriptionPurchaseToProjection(s) ).toList();
+        List<IScriptPurchaseProjection> scripts = userEntity.getScriptsPurchases().stream()
+            .map( s -> scriptPurchaseToProjection(s) ).toList();
 
         user.setSubscriptionsPurchases(subs);
+        user.setScriptsPurchases(scripts);
         user.setUpdatedAt(userEntity.getUpdatedAt());
 
         return user;
     }
 
-    ISubscriptionPurchaseProjection subscriptionPurchaseToProjection( SubscriptionPurchaseEntity entity ){
+    public IScriptPreviewProjection scriptToProjectionPreview( ScriptEntity entity ){
+        if( entity == null ) return null;
+        ScriptPreviewDto dto = new ScriptPreviewDto();
+
+        dto.setDescription(entity.getDescription());
+        dto.setId(entity.getId());
+        dto.setIsAvailableInSubscription(entity.getIsAvailableInSubscription());
+        dto.setName(entity.getName());
+        dto.setPrice(entity.getPrice());
+
+        return dto;
+    }
+
+    public IScriptPurchaseProjection scriptPurchaseToProjection( ScriptPurchaseEntity entity ){
+        if( entity == null ) return null;
+        ScriptPurchaseDto dto = new ScriptPurchaseDto();
+
+        dto.setKey(
+            keyEntityToProjection(entity.getKey())
+        );
+        dto.setScript(
+            scriptToProjectionPreview(entity.getScript())
+        );
+        dto.setUuid(entity.getUuid());
+
+        return dto;
+    }
+
+    public ISubscriptionPurchaseProjection subscriptionPurchaseToProjection( SubscriptionPurchaseEntity entity ){
         if( entity == null ) return null;
         SubscriptionPurchaseDto dto = new SubscriptionPurchaseDto();
 
