18c20ead5d81e3e5dd646a121c9a095a636f8c45
DevCompleteStudios <jorge.yael333@gmail.com>
2024-11-10 10:32:47 -0600

    Cors agregado

--- src/main/java/com/devstudios/store/devstudios_store_server/config/SecurityConfig.java
+++ src/main/java/com/devstudios/store/devstudios_store_server/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.devstudios.store.devstudios_store_server.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -7,6 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.devstudios.store.devstudios_store_server.infrastructure.services.JwtValidationFilter;
 
@@ -37,9 +41,22 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
             )
             .csrf( config -> config.disable() )
+            .cors( config -> config.configurationSource(corsConfigurationSource()) )
             .addFilterBefore(validationFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 
+
+    @Bean
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*")); // Permite todos los encabezados
+        configuration.setAllowCredentials(true); // Permite el envío de credenciales
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
